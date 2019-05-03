78abbfec056b2f4962d1ff01f0d14a8cc3ac740b
michele <michele.tessari12@gmail.com>
2019-05-03 19:38:43 +0200

    aggiunta grafica tasti premuti

--- app/src/main/java/com/tessari/jamrec/MainActivity.java
+++ app/src/main/java/com/tessari/jamrec/MainActivity.java
@@ -11,15 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ActionBar actionbar = getSupportActionBar();
-        //Toolbar tb = (Toolbar) findViewById(R.id.toolbar);
 
-        actionbar.setDisplayShowHomeEnabled(false);
-        actionbar.setDisplayShowTitleEnabled(false);
-        actionbar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-        actionbar.setDisplayShowCustomEnabled(true);
-//        setSupportActionBar(tb);
-        actionbar.setCustomView(R.layout.custom_action_bar_layout);
     }
 
 }
