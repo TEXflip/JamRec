5e9ba56c94a1c1885d8e9c320248ca2534d79c6f
michele <michele.tessari12@gmail.com>
2019-05-06 11:18:05 +0200

    aggiunto controllo permesso di registrazione

--- app/src/main/java/com/tessari/jamrec/MainActivity.java
+++ app/src/main/java/com/tessari/jamrec/MainActivity.java
@@ -1,5 +1,9 @@
 package com.tessari.jamrec;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -11,7 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 1);
     }
 
 }
