10b91137eb1c756509418305c6adc86e12d1acbe
Michele <michele.tessari12@gmail.com>
2019-05-11 12:22:48 +0200

    aggiunto classer Recorder

--- app/src/main/java/com/tessari/jamrec/MainActivity.java
+++ app/src/main/java/com/tessari/jamrec/MainActivity.java
@@ -2,33 +2,17 @@ package com.tessari.jamrec;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.media.AudioFormat;
-import android.media.AudioManager;
-import android.media.AudioRecord;
-import android.media.AudioTrack;
-import android.media.MediaRecorder;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.ActionBar;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-
-import java.nio.ByteBuffer;
 
 public class MainActivity extends AppCompatActivity {
 
 //    View bottomBar = findViewById(R.id.bottom_bar);
 //    Button recButton = (Button) findViewById(R.id.recButton);
-    int recording = 0;
-    AudioRecord recorder = null;
-//    MediaRecorder mediaRecorder;
-    Thread recordingThread;
-    boolean isRecording = false;
-    int bufferSize = 512;
+    Recorder rec;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,64 +20,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED)
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 1);
+        rec = new Recorder();
     }
 
-    public void recButtonOnClick(View v){
+    public void recButtonOnClick(View v) {
 
-        if(recording % 2 == 0) {
-            recorder = new AudioRecord.Builder()
-                    .setAudioSource(MediaRecorder.AudioSource.VOICE_COMMUNICATION)
-                    .setAudioFormat(new AudioFormat.Builder()
-                            .setEncoding(AudioFormat.ENCODING_PCM_16BIT)//cambiabile per mp3??
-                            .setSampleRate(44100)
-                            .setChannelMask(AudioFormat.CHANNEL_IN_MONO)
-                            .build())
-                    .setBufferSizeInBytes(bufferSize)
-                    .build();
-            recorder.startRecording();
-            isRecording = true;
-            recordingThread = new Thread(new Runnable()
-
-            {
-                public void run() {
-                    byte data[] = new byte[bufferSize];
-                    while (isRecording) {
-
-                        recorder.read(data, 0, bufferSize);
-                        send(data);
-
-                    }
-                }
-
-            });
-            recordingThread.start();
+        if (!rec.isRecording()) {
+            rec.startToRec();
+        } else {
+            rec.stop();
         }
-        else {
-            isRecording = false;
-            recorder.stop();
-            recorder.release();
-            recorder = null;
-            recordingThread = null;
-        }
-        recording++;
     }
-
-    private void send(byte[] data) {
-
-        Integer minBufferSize = AudioTrack.getMinBufferSize(8000,
-                AudioFormat.CHANNEL_OUT_MONO,
-                AudioFormat.ENCODING_PCM_16BIT);
-
-        AudioTrack at = new AudioTrack(AudioManager.STREAM_MUSIC, 8000,
-                AudioFormat.CHANNEL_OUT_MONO,
-                AudioFormat.ENCODING_PCM_16BIT, 4096,
-                AudioTrack.MODE_STREAM);
-
-        at.play();
-        at.write(data, 0, bufferSize);
-        at.stop();
-        at.release();
-
-    }
-
 }
---
+++ app/src/main/java/com/tessari/jamrec/Recorder.java
@@ -0,0 +1,68 @@
+package com.tessari.jamrec;
+
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioRecord;
+import android.media.AudioTrack;
+import android.media.MediaRecorder;
+
+public class Recorder {
+    private AudioRecord recorder = null;
+    private AudioTrack at = null;
+    private boolean isRecording = false;
+    private Thread recordingThread;
+    private int bufferSize = 1024,
+            sampleRate = 44100,
+            audio_encoding = AudioFormat.ENCODING_PCM_16BIT,//cambiabile per mp3??
+            audio_channel_in = AudioFormat.CHANNEL_IN_MONO,
+            audio_channel_out = AudioFormat.CHANNEL_OUT_MONO;
+
+    public Recorder() {
+        recorder = new AudioRecord.Builder()
+                .setAudioSource(MediaRecorder.AudioSource.VOICE_COMMUNICATION)
+                .setAudioFormat(new AudioFormat.Builder()
+                        .setEncoding(audio_encoding)
+                        .setSampleRate(sampleRate)
+                        .setChannelMask(audio_channel_in)
+                        .build())
+                .setBufferSizeInBytes(bufferSize)
+                .build();
+
+        at = new AudioTrack(AudioManager.STREAM_MUSIC, sampleRate,
+                audio_channel_out,
+                audio_encoding, bufferSize,
+                AudioTrack.MODE_STREAM);
+    }
+
+    public void startToRec() {
+        recorder.startRecording();
+        isRecording = true;
+        recordingThread = new RecordingThread();
+        at.play();
+        recordingThread.start();
+    }
+
+    public void stop() {
+        isRecording = false;
+        recorder.stop();
+        recorder.release();
+        recorder = null;
+        recordingThread = null;
+        at.stop();
+        at.release();
+    }
+
+    public boolean isRecording(){
+        return isRecording;
+    }
+
+    private class RecordingThread extends Thread {
+        public void run() {
+            short[] data = new short[bufferSize];
+            while (isRecording) {
+                recorder.read(data, 0, bufferSize, AudioRecord.READ_BLOCKING);
+                at.write(data, 0, bufferSize, AudioTrack.WRITE_BLOCKING);
+            }
+        }
+    }
+}
